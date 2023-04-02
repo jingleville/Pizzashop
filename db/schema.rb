dd564a1a45d39be87da0e2b6f7fe1757f22424b1
jingleville <andrewgavrick@yandex.ru>
2023-04-02 11:37:36 +0300

    ceed db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_02_073521) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_02_082152) do
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "decription"
