fa2a3b706e312b3e771536bd09ac95bb03997969
jingleville <andrewgavrick@yandex.ru>
2023-04-02 18:14:06 +0300

    Added - button to decrease amount of pizzas

--- public/app.js
+++ public/app.js
@@ -1,14 +1,30 @@
-function smth() 
+function total()
 {
-
-  alert("Hello from button");
+  var result = 0;
+  Object.keys(window.localStorage).forEach(function(key){
+    result = result + localStorage.getItem(key) * 1
+  });
+  return result
 }
 
-function add_to_cart(id)
+function add_to_cart(id, modifyer)
 {
   var key = "product_" + id
 
   var x = window.localStorage.getItem(key);
-  x = x * 1 + 1;
+  if (modifyer == '+') {
+    var mod = 1;
+  } else {
+    var mod = -1;
+  }
+
+  x = x * 1 + mod;
+
+  if (x < 0) {
+    x = 0;
+  }
+  
   window.localStorage.setItem(key, x);
+  console.log('amount of ' + key + ' is ' + x);
+  console.log(total());
 }
